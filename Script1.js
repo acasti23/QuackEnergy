7e8ab111deee8cec5f00761b5f8ce656c10ae9d4
nicoleeeeu <hnvinuya@gmail.com>
2024-03-03 11:28:07 -0800

    Update Script1.js

--- Script1.js
+++ Script1.js
@@ -8,9 +8,10 @@ $(document).ready(function () {
         var UserIn = PasswordField.value;
 
         if (taskNum == 1) {
-            if (UserIn == "Workshop Room") {
-                document.getElementById('task-num').innerText = "#2 I still need cloth";
-                document.getElementById('task-desc').innerText = "This is Task 2";
+            if (UserIn == "24 times") {
+                document.getElementById('task-num').innerText = "#2 - I still need clothes";
+                document.getElementById('task-desc').innerText = "What foods does a duck prefer eating with soup?";
+                Task1.src = 'Images/Task1_icon.png';
                 duckIcon.src = 'Images/Task1_Complete.png';//Changes Image
                 PasswordField.value = "";
                 taskNum++;
@@ -18,18 +19,20 @@ $(document).ready(function () {
                 alert("Wrong Answer");
             }
         } else if (taskNum == 2) {
-            if (UserIn == "Room") {
-                document.getElementById('task-num').innerText = "#3 Don't foget laptp";
-                document.getElementById('task-desc').innerText = "This is Task 3";
+            if (UserIn == "Quackers") {
+                document.getElementById('task-num').innerText = "#3 - Don't forget my laptop";
+                document.getElementById('task-desc').innerText = "There are two ducks in front of a duck, two ducks behind a duck and a duck in the middle. How many ducks are there?";
+                Task2.src = 'Images/Task2_icon.png';
                 PasswordField.value = "";
                 taskNum++;
             } else {
                 alert("Wrong Answer");
             }
         } else if (taskNum == 3) {
-            if (UserIn == "Sleep Room") {
+            if (UserIn == "Three") {
                 document.getElementById('task-num').innerText = "Task completed!";
                 document.getElementById('task-desc').innerText = "Congratulations!";
+                Task3.src = 'Images/Task3_icon.png';
                 errorMessage.style.color = "black";
                 taskNum++;
             } else {
