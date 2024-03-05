fb17379b576e5bc2956636c064d386bb5154f94b
acasti23 <acastillo82100@gmail.com>
2024-03-04 18:42:50 -0800

    Function fixed

--- Script1.js
+++ Script1.js
@@ -8,9 +8,11 @@ $(document).ready(function () {
         var UserIn = PasswordField.value;
 
         if (taskNum == 1) {
-            if (UserIn == "24 times") {
-                document.getElementById('task-num').innerText = "#2 - I still need clothes";
-                document.getElementById('task-desc').innerText = "What foods does a duck prefer eating with soup?";
+            if (UserIn.toLowerCase() == "e7-210" || UserIn.toLowerCase() == "e7 210") {//Enginnering Club
+                alert("Great Job");
+                window.scrollTo({ top: 0, behavior: 'smooth' });
+                document.getElementById('task-num').innerText = "2 Engineering Club";
+                document.getElementById('task-desc').innerText = "Where are the meeting hosted";
                 Task1.src = 'Images/Task1_icon.png';
                 duckIcon.src = 'Images/Task1_Complete.png';//Changes Image
                 PasswordField.value = "";
@@ -18,19 +20,36 @@ $(document).ready(function () {
             } else {
                 alert("Try Again");
             }
-        } else if (taskNum == 2) {
-            if (UserIn == "Quackers") {
-                document.getElementById('task-num').innerText = "#3 - Don't forget my laptop";
-                document.getElementById('task-desc').innerText = "There are two ducks in front of a duck, two ducks behind a duck and a duck in the middle. How many ducks are there?";
+        } else if (taskNum == 2) {//Veteran Club
+            if (UserIn.toLowerCase() == "e7-101" || UserIn.toLowerCase() == "e7 101") {
+                alert("Great Job");
+                window.scrollTo({ top: 0, behavior: 'smooth' });
+                document.getElementById('task-num').innerText = "3 Veteran's Club";
+                document.getElementById('task-desc').innerText = "Where is the Veteran Resource Center Located";
                 Task2.src = 'Images/Task2_icon.png';
+                duckIcon.src = 'Images/Task2_Complete.png';//Changes Image
                 PasswordField.value = "";
                 taskNum++;
             } else {
                 alert("Try Again");
             }
-        } else if (taskNum == 3) {
-            if (UserIn == "Three") {
-                window.location.href = '/end.html';
+        } else if (taskNum == 3) {//Computer Club
+            if (UserIn.toLowerCase() == "d7-a" || UserIn.toLowerCase() == "d7 a") {
+                alert("Great Job");
+                window.scrollTo({ top: 0, behavior: 'smooth' });
+                document.getElementById('task-num').innerText = "4 Huskies Computer Club";
+                document.getElementById('task-desc').innerText = "Where do they host their meetings";
+                Task2.src = 'Images/Task3_icon.png';
+                duckIcon.src = 'Images/Task3_Complete.png';//Changes Image
+                PasswordField.value = "";
+                taskNum++;
+            } else {
+                alert("Try Again");
+            }
+        } else if (taskNum == 4) { 
+            if (UserIn.toLowerCase() == "e7-318" || UserIn.toLowerCase() == "e7 318") {
+                window.location.href = 'end.html';
+                
             } else {
                 alert("Try Again");
             }
--- loginJS.js
+++ loginJS.js
@@ -1,18 +1,16 @@
-// JavaScript source code
 $(document).ready(function () {
     const logInBtn = document.getElementById('submit-button');
-    const userNameField = document.getElementById('username-input')
-    const passwordField = document.getElementById('password-input')
-    logInBtn.addEventListener('click', function () {
+    const userNameField = document.getElementById('username-input');
+    const passwordField = document.getElementById('password-input');
 
+    logInBtn.addEventListener('click', function () {
         var userName = userNameField.value;
         var password = passwordField.value;
+
         if (userName == "SuperDucky" && password == "DuckyMomo123") {
-            window.location.href = 'instructions.html';
-        }
-        else {
+            window.location.href = '/instructions.html';
+        } else {
             alert("Wrong Password");
         }
     });
-
 });
